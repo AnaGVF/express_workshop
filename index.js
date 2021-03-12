07b6d2b3e24dd2d416d3ff89c9fd4dd8644c37a3
Ana Vassallo <aniavassallo@gmail.com>
2021-03-11 18:08:57 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+/*
+Verbos HTTP
+GET
+POST
+PATCH
+PUT
+DELETE
+*/ 
+
+const express = require('express');
+const app = express();
+
+app.get("/", (req, res, next) => {
+    res.status(200);
+    res.send("Bienvenido");
+});
+
+app.listen(3000, () => {
+    console.log("Server is running...");
+});
